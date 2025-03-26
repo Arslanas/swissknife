f0ee8ec8ed43957e38c735c9cb74a04a387353fc
Arslan <ovezberdyevarslan@gmail.com>
2025-03-26 20:56:24 +0900

    get files by keyword

--- src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
+++ src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
@@ -2,10 +2,13 @@ package com.arslan.swissknife.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.PsiSearchHelper
+import com.intellij.psi.search.UsageSearchContext
 import java.util.*
+import kotlin.collections.HashMap
 
 class SearchKeywordInFiles : AnAction() {
 
@@ -17,6 +20,33 @@ class SearchKeywordInFiles : AnAction() {
             return
         }
 
+
+        val psiSearchHelper = PsiSearchHelper.getInstance(project)
+        val scope = GlobalSearchScope.projectScope(project)
+
+        val keyword = "main"
+        val caseSensitivity = false
+
+        val map = HashMap<String, Int>()
+        val visited = HashSet<String>()
+
+        psiSearchHelper.processElementsWithWord(
+            { element: PsiElement, offset: Int ->
+                val file = element.containingFile?.virtualFile
+                if (file != null) {
+                    if (visited.contains(file.name)) return@processElementsWithWord true
+                    map.put(file.name, map.getOrDefault(file.name, 0) + 1)
+                    visited.add(file.name)
+                }
+                true // Continue search
+            },
+            scope,
+            keyword,
+            UsageSearchContext.ANY,
+            caseSensitivity
+        )
+
+        map.forEach {  key, value -> println("$key: $value") }
     }
 
 
