0b4c35c97361a5fb1ebf0b11042e67f69b03cb7f
Arslan <ovezberdyevarslan@gmail.com>
2025-03-30 14:53:01 +0900

    get files by keyword

--- src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
+++ src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
@@ -13,8 +13,11 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.PsiSearchHelper
 import com.intellij.psi.search.UsageSearchContext
 import com.intellij.find.impl.FindInProjectUtil
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.fileEditor.FileEditorLocation
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.vfs.findPsiFile
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.*
 import com.intellij.util.Processor
@@ -36,24 +39,63 @@ class SearchKeywordInFiles : AnAction() {
             isWholeWordsOnly = false
             isFindAll = true
             isFindAllEnabled = true
+            searchContext = FindModel.SearchContext.ANY
         }
 
-        val usageViewPresentation = FindInProjectUtil.setupViewPresentation(true, findModel)
-        usageViewPresentation.tabText = "Find usages for \"$keyword\""
+        // Store unique files that contain the keyword
+        val uniqueFiles = mutableSetOf<VirtualFile>()
+
+        val usageViewPresentation = UsageViewPresentation().apply {
+            searchString = keyword
+            tabText = "Files containing \"$keyword\""
+            isOpenInNewTab = true
+            isCodeUsages = false  // Forces flat list view
+            isMergeDupLinesAvailable = false // Prevents merging similar results
+            isShowReadOnlyStatusAsRed = false
+            isShowCancelButton = true
+        }
 
         val processPresentation = FindUsagesProcessPresentation(usageViewPresentation).apply {
             isShowPanelIfOnlyOneUsage = false
         }
 
-        val usageList = mutableListOf<Usage>()
+//        val usageList = mutableListOf<Usage>()
 
         val processor = Processor<UsageInfo> { usageInfo ->
             println(usageInfo)
-            usageList.add(UsageInfo2UsageAdapter(usageInfo))
+            usageInfo.virtualFile?.let { uniqueFiles.add(it) }
+//            usageList.add(UsageInfo2UsageAdapter(usageInfo))
             true
         }
 
         FindInProjectUtil.findUsages(findModel, project, processor, processPresentation)
+
+        // Convert file results into Usage objects
+        val usageList = uniqueFiles.map { file ->
+            object : Usage {
+                override fun getPresentation() = object : UsagePresentation {
+                    override fun getText() = arrayOf(TextChunk(TextAttributes(), file.presentableUrl))  // Show only file name
+                    override fun getPlainText(): String {
+                        return file.presentableUrl
+                    }
+
+                    override fun getIcon() = null
+                    override fun getTooltipText() = file.presentableUrl
+                }
+
+                override fun isValid() = true
+                override fun isReadOnly() = false
+                override fun getLocation(): FileEditorLocation? {
+                    TODO("Not yet implemented")
+                }
+
+                override fun selectInEditor() {}
+
+                override fun highlightInEditor() {}
+
+                override fun navigate(requestFocus: Boolean) { /* Open file on click */ }
+        } }
+
 
         if (usageList.isNotEmpty()) {
             val usageViewManager = UsageViewManager.getInstance(project)
