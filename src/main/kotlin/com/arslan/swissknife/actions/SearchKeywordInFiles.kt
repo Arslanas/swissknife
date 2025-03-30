676527cab86d2d4ad4b4aaeb64e64172eae3f774
Arslan <ovezberdyevarslan@gmail.com>
2025-03-30 09:55:38 +0900

    get files by keyword

--- src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
+++ src/main/kotlin/com/arslan/swissknife/actions/SearchKeywordInFiles.kt
@@ -12,7 +12,6 @@ import com.intellij.psi.search.UsageSearchContext
 
 class SearchKeywordInFiles : AnAction() {
 
-
     override fun actionPerformed(e: AnActionEvent) {
 
         val project = e.project ?: run{
@@ -59,6 +58,54 @@ class SearchKeywordInFiles : AnAction() {
 
         map.forEach {  key, value -> println("$key: $value") }
     }
+
+
+//    override fun actionPerformed(e: AnActionEvent) {
+//
+//        val project = e.project ?: run{
+//            Messages.showErrorDialog("No project found", "Error")
+//            return
+//        }
+//
+//
+//        val psiSearchHelper = PsiSearchHelper.getInstance(project)
+//        val scope = GlobalSearchScope.projectScope(project)
+//
+//        val keyword = "main"
+//        val caseSensitivity = false
+//
+//        val visited = HashSet<VirtualFile>()
+//
+//        psiSearchHelper.processElementsWithWord(
+//            { element: PsiElement, offset: Int ->
+//                val file = element.containingFile?.virtualFile
+//                if (file != null) {
+//                    if (visited.contains(file)) return@processElementsWithWord true
+//                    visited.add(file)
+//                }
+//                true // Continue search
+//            },
+//            scope,
+//            keyword,
+//            UsageSearchContext.ANY,
+//            caseSensitivity
+//        )
+//
+//
+//        val map = HashMap<String, Int>()
+//
+//        visited.forEach({ file ->
+//            val content = VfsUtilCore.loadText(file) // Read file content
+//            val count = countOccurrences(content, keyword)
+//            if (count > 0) {
+//                map[file.path] = count
+//            }
+//        })
+//
+//
+//
+//        map.forEach {  key, value -> println("$key: $value") }
+//    }
 
 
     private fun countOccurrences(text: String, keyword: String): Int {
