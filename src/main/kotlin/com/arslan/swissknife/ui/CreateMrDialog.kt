c79c5ce3fac71408e3267ed5f055f27e6764d761
Arslan <ovezberdyevarslan@gmail.com>
2025-04-05 15:56:30 +0900

    Create MR window

--- src/main/kotlin/com/arslan/swissknife/ui/CreateMrDialog.kt
+++ src/main/kotlin/com/arslan/swissknife/ui/CreateMrDialog.kt
@@ -1,6 +1,5 @@
 package com.arslan.swissknife.ui
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.dsl.builder.Align
@@ -11,11 +10,11 @@ import javax.swing.*
 
 class CreateMrDialog(project: Project?) : DialogWrapper(project) {
 
-    var title = ""
+    var mrTitle = ""
     private val options = listOf("Option 1", "Option 2", "Option 3")
 
     init {
-        title = "Create Mr Dialog"
+        mrTitle = "Create Mr Dialog"
         init()
     }
 
@@ -31,7 +30,7 @@ class CreateMrDialog(project: Project?) : DialogWrapper(project) {
                     .resizableColumn()
                     .align(Align.FILL)
                     .focused()
-                    .bindText({ title}, {title = it})
+                    .bindText({ mrTitle}, {mrTitle = it})
             }
         }
     }
