09c5edcc1440ed5d2858cd6ef1da8bd27ec6ee20
Arslan <ovezberdyevarslan@gmail.com>
2025-04-06 10:57:11 +0900

    Create MR window

--- js/Create_MR.js
+++ js/Create_MR.js
@@ -1,7 +1,9 @@
-const details = {
+var details = {
     title : 'Some title',
     targetBranch : 'origin/release/capg.2025.5.0'
 }
+
+if (1 === 1) details = null
 
 console.log(`Create MR for details 
     branch : ${details.targetBranch} 
