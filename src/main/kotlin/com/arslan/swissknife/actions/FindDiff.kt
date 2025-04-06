62d09ddf0bbee2a7fdcea6d58e68e4ba69aaa61c
Arslan <ovezberdyevarslan@gmail.com>
2025-04-06 15:27:57 +0900

    Diff checker

---
+++ src/main/kotlin/com/arslan/swissknife/actions/FindDiff.kt
@@ -0,0 +1,68 @@
+package com.arslan.swissknife.actions
+
+import com.intellij.diff.DiffContentFactory
+import com.intellij.diff.DiffManager
+import com.intellij.diff.requests.SimpleDiffRequest
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.Messages
+import com.intellij.ui.dsl.builder.Align
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.panel
+import javax.swing.JComponent
+
+class FindDiff : AnAction(){
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: run{
+            Messages.showErrorDialog("No project found", "Error")
+            return
+        }
+
+        DiffDialog(project).show()
+    }
+
+    class DiffDialog(private val project: Project) : DialogWrapper(project) {
+
+        private var a = ""
+        private var b = ""
+
+        init {
+            title = "Compare Texts"
+            init()
+        }
+
+        override fun createCenterPanel(): JComponent {
+            return panel {
+                row("A") {
+                    textArea().align(Align.FILL).bindText({a}, {a = it})
+                }
+                row("B") {
+                    textArea().align(Align.FILL).bindText({b}, {b = it})
+                }
+            }
+        }
+
+        override fun doOKAction() {
+            super.doOKAction()
+
+            val leftText = a
+            val rightText = b
+
+            val contentFactory = DiffContentFactory.getInstance()
+            val leftContent = contentFactory.create(project, leftText)
+            val rightContent = contentFactory.create(project, rightText)
+
+            val request = SimpleDiffRequest(
+                "Text Comparison",
+                leftContent, rightContent,
+                "Left", "Right"
+            )
+
+            DiffManager.getInstance().showDiff(project, request)
+        }
+    }
+
+}
