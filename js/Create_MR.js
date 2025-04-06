e1dbbc8163fdcb3fd6641df04d9c37d85f23f686
Arslan <ovezberdyevarslan@gmail.com>
2025-04-06 10:46:10 +0900

    Create MR window

---
+++ js/Create_MR.js
@@ -0,0 +1,9 @@
+const details = {
+    title : 'Some title',
+    targetBranch : 'origin/release/capg.2025.5.0'
+}
+
+console.log(`Create MR for details 
+    branch : ${details.targetBranch} 
+    title : ${details.title}`)
+
