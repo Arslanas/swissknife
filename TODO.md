f89b3a8036bbdef0dfa0b585662d3cc946b57d60
Arslan <ovezberdyevarslan@gmail.com>
2025-04-09 20:52:57 +0900

    Some text

--- TODO.md
+++ TODO.md
@@ -1,14 +1,14 @@
 
 ## To do
+1. open MR in browser
 1. rebase on release branch selected
 1. Open jira folder - add analysis-xxxx.xlsx  and notes-xxxx.txt files by default if not exist
-1. Bump up version for release branch for selected components. Also select target release.yml. Consider update of core component. 
-1. Get list of MRs where I made a comment but not yet resolved a thread.
 1. pluggable modifier of the selected code. Copy text from editor, call python/js scripts to transform the selected text, paste transformed text to editor
-1. open MR in browser
 1. lookup files by usage
 1. show usages of JPA interface by clicking 
 1. open related unit test class on right click   
+1. Bump up version for release branch for selected components. Also select target release.yml. Consider update of core component.
+1. Get list of MRs where I made a comment but not yet resolved a thread.
 1. create MR for release branch - input title (prepopulated by Jira name)
 1. convert sql extracted from DB to java JPA entity class
 1. on click do create entry in changelog and update + rollback scripts
---
+++ src/main/kotlin/com/arslan/swissknife/actions/OpenGitLabMRAction.kt
@@ -0,0 +1,38 @@
+package com.arslan.swissknife.actions
+
+import com.intellij.ide.BrowserUtil
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+import com.intellij.vcs.log.VcsLogDataKeys
+import com.intellij.vcs.log.data.AbstractDataGetter.Companion.getCommitDetails
+import com.intellij.vcs.log.data.VcsLogData
+
+class OpenGitLabMRAction : AnAction("Open Merge Request on GitLab") {
+
+    override fun update(e: AnActionEvent) {
+        val commits = e.getData(VcsLogDataKeys.VCS_LOG_COMMIT_SELECTION)
+        e.presentation.isEnabledAndVisible = !commits?.commits.isNullOrEmpty()
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val commits = e.getData(VcsLogDataKeys.VCS_LOG_COMMIT_SELECTION)
+        if (commits?.commits.isNullOrEmpty()) return
+
+        val commitHash = commits?.commits?.first()?.hash?.asString()
+
+        val commitDetails =
+            (e.getData(VcsLogDataKeys.VCS_LOG_DATA_PROVIDER) as VcsLogData).commitDetailsGetter.getCommitDetails(commits?.ids!!)
+
+        commitDetails.first().id.asString() // hash
+        commitDetails.first().author // author
+        commitDetails.first().fullMessage // message - could extract jira number and open in browser
+
+        // You'd likely want to derive these dynamically
+        val gitlabBaseUrl = "https://gitlab.com"
+        val projectPath = "your-group/your-repo" // Could be from remote URL
+        val mrUrl = "$gitlabBaseUrl/$projectPath/-/commit/$commitHash"
+
+        Messages.showInfoMessage(mrUrl, "Merge Request on GitLab")
+    }
+}
