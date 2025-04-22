9403f6c3e3ad28f556092b17a401b8e1d6a66a46
Arslan <ovezberdyevarslan@gmail.com>
2025-04-23 07:32:56 +0900

    Get JPA save method usages

--- src/main/kotlin/com/arslan/swissknife/intentions/ShowSave.kt
+++ src/main/kotlin/com/arslan/swissknife/intentions/ShowSave.kt
@@ -6,14 +6,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.PsiShortNamesCache
-import com.intellij.psi.search.searches.MethodReferencesSearch
 import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.refactoring.util.RefactoringChangeUtil.getQualifierClass
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.*
-import com.intellij.util.Query
 
 class ShowSave : IntentionAction {
     override fun getText(): String = "CAPG: show save/saveAll operations"
@@ -34,44 +30,7 @@ class ShowSave : IntentionAction {
         return isJPAInterface
     }
 
-    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-        val offset = editor!!.caretModel.offset
-        val element = file!!.findElementAt(offset) ?: return
-        val psiClass = PsiTreeUtil.getParentOfType(element, PsiClass::class.java) ?: return
-
-        val methods = psiClass.allMethods.filter {
-            it.name == "save" || it.name == "saveAll"
-        }
-
-        if (methods.isEmpty()) {
-            Messages.showInfoMessage(project, "No save/saveAll methods found in this interface.", "ShowSave")
-            return
-        }
-
-        // Find usages of save/saveAll methods
-        val usageInfos = mutableListOf<UsageInfo>()
-        val searchScope = GlobalSearchScope.projectScope(project)
-
-        for (method in methods) {
-            val query = MethodReferencesSearch.search(method, searchScope, true)
-            query.forEach { ref ->
-                if (psiClass.qualifiedName == getQualifiedClass(ref)) usageInfos.add(UsageInfo(ref.element))
-            }
-        }
-
-        if (usageInfos.isEmpty()) {
-            Messages.showInfoMessage(project, "No usages of save/saveAll found.", "ShowSave")
-        } else {
-            UsageViewManager.getInstance(project).showUsages(
-                UsageTarget.EMPTY_ARRAY,
-                usageInfos.map { UsageInfo2UsageAdapter(it) }.toTypedArray(),
-                UsageViewPresentation().apply { tabText = "Save/SaveAll Usages" }
-            )
-        }
-    }
-
-
-    private fun lookByClassFirst(project: Project, editor: Editor?, file: PsiFile?){
+    override fun invoke(project: Project, editor: Editor?, file: PsiFile?){
         val offset = editor!!.caretModel.offset
         val element = file!!.findElementAt(offset) ?: return
         val psiClass = PsiTreeUtil.getParentOfType(element, PsiClass::class.java) ?: return
@@ -91,24 +50,36 @@ class ShowSave : IntentionAction {
 
             val variable = PsiTreeUtil.getParentOfType(element, PsiVariable::class.java)
 
+
+            val usageInfos = mutableListOf<UsageInfo>()
+
             if (variable != null) {
                 val variableUsages = ReferencesSearch.search(variable, GlobalSearchScope.projectScope(project))
                 for (usage in variableUsages) {
-                    val element = usage.element
-                    val parent = element.parent
+                    val usageElement = usage.element
+                    val usageParent = usageElement.parent
 
-                    if (parent is PsiReferenceExpression &&
-                        parent.parent is PsiMethodCallExpression
-                    ) {
-                        val methodCall = parent.parent as PsiMethodCallExpression
+                    if (usageParent.parent is PsiMethodCallExpression) {
+                        val methodCall = usageParent.parent as PsiMethodCallExpression
                         val methodName = methodCall.methodExpression.referenceName
-                        println("Method call on variable: $methodName → ${methodCall.text}")
-
-                        // TODO : collect and show usages
+                        if (methodName == "save" || methodName == "saveAll") {
+                            usageInfos.add(UsageInfo(methodCall))
+                        }
                     }
                 }
             } else {
                 println("Other usage: ${element.text}")
+            }
+
+
+            if (usageInfos.isEmpty()) {
+                Messages.showInfoMessage(project, "No usages of save/saveAll found.", "ShowSave")
+            } else {
+                UsageViewManager.getInstance(project).showUsages(
+                    UsageTarget.EMPTY_ARRAY,
+                    usageInfos.map { UsageInfo2UsageAdapter(it) }.toTypedArray(),
+                    UsageViewPresentation().apply { tabText = "Save/SaveAll Usages" }
+                )
             }
         }
     }
