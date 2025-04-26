caa81942fdc3d568e467bf4812ac8a2c482ac4fe
Arslan <ovezberdyevarslan@gmail.com>
2025-04-26 09:59:46 +0900

    Get JPA save method usages

--- src/main/kotlin/com/arslan/swissknife/intentions/GoToRepoIntention.kt
+++ src/main/kotlin/com/arslan/swissknife/intentions/GoToRepoIntention.kt
@@ -13,9 +13,9 @@ import com.intellij.util.PsiNavigateUtil
 
 class GoToRepoIntention : PsiElementBaseIntentionAction() {
 
-    override fun getFamilyName(): String = "Go To Repo"
+    override fun getFamilyName(): String = "CAPG"
 
-    override fun getText(): String = "Go To Repository Interface"
+    override fun getText(): String = "CAPG : Go to repository"
 
     override fun isAvailable(project: Project, editor: Editor, element: PsiElement): Boolean {
         val psiClass = element.parent as? PsiClass ?: return false
@@ -50,10 +50,21 @@ class GoToRepoIntention : PsiElementBaseIntentionAction() {
         if (!this.isInterface) return false
 
         return this.extendsListTypes.any { type ->
-            val base = type.resolve()?.qualifiedName
-            (base == "org.springframework.data.repository.CrudRepository" ||
-             base == "org.springframework.data.jpa.repository.JpaRepository")
-                    && (type as? PsiClassType)?.parameters?.firstOrNull()?.resolve()?.qualifiedName == entityQualifiedName
+            if (type !is PsiClassType) return@any false
+
+            val resolvedClass = type.resolve()
+            val qName = resolvedClass?.qualifiedName ?: return@any false
+
+            if (qName != "org.springframework.data.repository.CrudRepository" &&
+                qName != "org.springframework.data.jpa.repository.JpaRepository") return@any false
+
+            val typeArguments = type.parameters
+            if (typeArguments.isEmpty()) return@any false
+
+            val entityType = typeArguments[0]
+            val entityPsiClass = (entityType as? PsiClassType)?.resolve() ?: return@any false
+
+            return@any entityPsiClass.qualifiedName == entityQualifiedName
         }
     }
 }
