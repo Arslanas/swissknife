d28e024e8c720339fe1417011d7997bf1a2db759
Arslan Ovezberdyev <ovezberdyevarslan@gmail.com>
2025-12-17 11:06:16 +0900

    master : Multi caret fix

--- deploy.bash
+++ deploy.bash
@@ -1,10 +1,11 @@
-source=/c/D/APPLICATIONS/IDE_PLUGINS/swissknife/build/distributions/swissknife-1.0-SNAPSHOT.zip
-destination=/c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp/src/main/resources/swissknife-1.0-SNAPSHOT.zip
+source=/mnt/c/D/APPLICATIONS/IDE_PLUGINS/swissknife/build/distributions/swissknife-1.0-SNAPSHOT.zip
+destination=/mnt/c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp/src/main/resources/swissknife-1.0-SNAPSHOT.zip
+echo "$(pwd)" $source to  $destination
 cp $source $destination
 echo copied $source to  $destination
 
-gitFolder=/c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp
+gitFolder=/mnt/c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp
 git -C $gitFolder add .
 git -C $gitFolder commit -m "Updated plugin"
 git -C $gitFolder push
-git -C /c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp push
+git -C /mnt/c/D/APPLICATIONS/IDE_PLUGINS/sendToTemp push
